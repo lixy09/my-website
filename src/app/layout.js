886e80e3c0ac895faddf7d34090a9ef8e83d0395
Anan Li <li0047@hz.nl>
2025-02-27 01:11:29 +0100

    updated component name

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,6 +1,6 @@
 import "./globals.css";
-import NavBar from "@/components/Navbar";
-import Footer from "@/components/Footer";
+import NavBar from "@/components/navbar";
+import Footer from "@/components/footer";
 
 export const metadata = {
     title: "Anan | Portfolio",
